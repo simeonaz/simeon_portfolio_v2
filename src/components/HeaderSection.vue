c19d1684c367a1680d12349eb110f7a01faff4a0
Siméon A. <simeonazogbonon@gmail.com>
2025-07-01 09:27:15 +0100

    changes

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -94,17 +94,18 @@ onMounted(() => {
           >
             <span
               class="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-12 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"
-              :class="{ 'w-16 bg-slate-200': activeLink === link.path }"
+              :class="{ 'w-16 bg-white': activeLink === link.path }"
             ></span>
             <span
               class="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200"
-              :class="{ 'text-slate-200': activeLink === link.path }"
+              :class="{ 'text-white': activeLink === link.path }"
               >{{ link.name }}</span
             >
           </a>
         </li>
       </ul>
 
+      <!-- Social media -->
       <ul class="ml-1 mt-8 flex items-center" aria-label="Social media">
         <li class="mr-5 shrink-0 text-xs" v-for="link in socialMediaLinks" :key="link.name">
           <a
