e75d075a4c422b1a0afe10caf4b7b6bce0ec3913
Siméon A. <simeonazogbonon@gmail.com>
2025-07-08 22:04:13 +0100

    minor change

--- src/components/ExperienceSection.vue
+++ src/components/ExperienceSection.vue
@@ -125,7 +125,6 @@ const experiences = [
         <a
           class="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 font-semibold text-slate-200 group/link text-base"
           href="/resume_simeon_dev.pdf"
-          target="_blank"
           rel="noreferrer noopener"
           aria-label="View Full Résumé (opens in a new tab)"
           ><span
